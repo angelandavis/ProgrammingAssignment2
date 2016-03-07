2e998dca59954bbb7ec18af6b8e8fe1ac480db29
angelandavis <angela.n.h.davis@gmail.com>
2016-03-07 01:23:40 -0600

    Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 - R Programming Notes
+##      makeCacheMatrix: This function creates a special "matrix" object that 
+##              can cache its inverse.
+##      cacheSolve: This function computes the inverse of the special "matrix" 
+##              returned by makeCacheMatrix above. 
+##      assign2: This function used by testing of the cache function. 
 
-## Write a short comment describing this function
+##      If the inverse has already been calculated (and the matrix has not 
+##              changed), then the cachesolve should retrieve the inverse from
+##              the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
+##      Computing the inverse of a square matrix can be done with the solve 
+##              function in R. For example, if X is a square invertible 
+##              matrix, then solve(X) returns its inverse.
 
+##      For this assignment, assume that the matrix supplied is always 
+##              invertible.
+
+makeCacheMatrix <- function(x=matrix()) {
+        x <<- y
+        inv <<- NULL
+        cachex<<-x
+        
+        set<-function(y){
+                x<<-y
+                inv<<-NULL
+        }
+        
+        get<- function()x
+        setinv <- function(inverse) inv<<-inverse
+        getinv <- function() inv
+        list(set=set,get=get,
+             setinv=setinv,getinv=getinv
+        )
+        
 }
 
+cacheSolve <- function(x,...) {
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data,...)
+        x$setinv(inv)
+        inv
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+assign2 = function(mat) {
+        ## create the cache Matrix
+        temp=makeCacheMatrix(mat)
+        
+        ## get the inverse and capture duration
+        start.time=Sys.time()
+        cacheSolve(temp)
+        dur=Sys.time() - start.time
+        print(dur)
+        
+        ## call for inverse again, should see cache, and capture duration to get cache
+        start.time=Sys.time()
+        cacheSolve(temp)
+        dur=Sys.time() - start.time
+        print(dur)
+        
 }
